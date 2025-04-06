2e53cd659ec1299a460f352e3f3fbf4c70529a57
Vivek Booshan <vivek.booshan@gmail.com>
2025-04-06 01:25:54 -0400

    remove semicolon from type max defines

--- vb.h
+++ vb.h
@@ -188,15 +188,15 @@ VB_STATIC_ASSERT(sizeof(u64) == 8);
 
 VB_STATIC_ASSERT(sizeof(usize) == sizeof(isize));
 
-#define  U8MAX UINT8_MAX;
-#define U16MAX UINT16_MAX;
-#define U32MAX UINT32_MAX;
-#define U64MAX UINT64_MAX;
+#define  U8MAX UINT8_MAX
+#define U16MAX UINT16_MAX
+#define U32MAX UINT32_MAX
+#define U64MAX UINT64_MAX
 
-#define  I8MAX INT8_MAX;
-#define I16MAX INT16_MAX;
-#define I32MAX INT32_MAX;
-#define I64MAX INT64_MAX;
+#define  I8MAX INT8_MAX
+#define I16MAX INT16_MAX
+#define I32MAX INT32_MAX
+#define I64MAX INT64_MAX
 
 #define F32_MIN 1.17549435e-38f
 #define F32_MAX 3.40282347e+38f
